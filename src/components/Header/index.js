ad40fc7c471b861798e314dc0676e5379314be54
Aisalkyn <aisalkyn3541@gmail.com>
2023-08-23 16:17:18 +0600

    header 2

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,23 +1,35 @@
-import React from 'react'
+import React from "react";
 import logo from "../../img/Logo.png";
-import { NavLink } from 'react-router-dom';
+import { NavLink } from "react-router-dom";
+import "./index.scss";
+import { AiOutlineSearch } from "react-icons/ai";
+import { PiHandbagSimple } from "react-icons/pi";
 
 const Header = () => {
   return (
     <div id="header">
-      <div className="header">
-        <div className="header--nav">
-          <img src={logo} alt="" />
-          <nav>
+        <div className="container">
+          <div className="header">
+            <div className="header--nav">
+            <img src={logo} alt="" />
+
             <NavLink>Categories</NavLink>
             <NavLink>Recent</NavLink>
             <NavLink>Books</NavLink>
             <NavLink>About Us</NavLink>
-          </nav>
+          </div>
+          <div className="header--icons">
+            <h3>
+              <AiOutlineSearch />
+            </h3>
+            <h3>
+              <PiHandbagSimple />{" "}
+            </h3>
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
 
-export default Header
+export default Header;
