aeaab3160675082549592c06cfe650218d168211
aisalkyn3541 <126472043+aisalkyn3541@users.noreply.github.com>
2023-08-23 16:21:47 +0600

    Header2 (#2)
    * asds
    
    * header 2
    
    ---------
    
    Co-authored-by: Aisalkyn <aisalkyn3541@gmail.com>

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import './index.scss'
 
 const Home = () => {
   return (
-    <div></div>
-  )
+    <div id="home">
+      <div className="container">
+        <div className="home">
+          <h1>Welcome to our Bookshop</h1>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Home
