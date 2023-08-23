74065e7102bc99f85ec2d866b6489f701a1d1401
Aisalkyn <aisalkyn3541@gmail.com>
2023-08-23 11:42:50 +0600

    asds

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import logo from "../../img/Logo.png";
+import { NavLink } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div></div>
-  )
+    <div id="header">
+      <div className="header">
+        <div className="header--nav">
+          <img src={logo} alt="" />
+          <nav>
+            <NavLink>Categories</NavLink>
+            <NavLink>Recent</NavLink>
+            <NavLink>Books</NavLink>
+            <NavLink>About Us</NavLink>
+          </nav>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Header
